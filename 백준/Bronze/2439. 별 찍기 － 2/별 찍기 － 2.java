2ab491505468c6ea3f97bd1308052ea8a30ee061
ChaeWon Lee <87990439+zzex3on@users.noreply.github.com>
2023-12-04 18:30:55 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 136 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        for(int i = 0; i < num; i++) {
+            for(int j = num-1; j > i; j--){
+                bw.write(" ");
+            }
+            for(int j = 0; j < i+1; j++){
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
