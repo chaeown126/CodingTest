24b8118aa5f237c1d78fc90fb5b62af5ca086eae
ChaeWon Lee <87990439+zzex3on@users.noreply.github.com>
2023-12-16 23:58:31 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 212 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int length = str.length();
+        
+        System.out.print(length);
+    }
+}
