b46d4ccef0b84eb982b24b31d6bdfecaa033bd4a
ChaeWon Lee <87990439+zzex3on@users.noreply.github.com>
2023-12-27 15:23:31 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 864 ms, Memory: 105060 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int pokemonIdx = 0;
+        Map<Integer, String> pokemonMap1 = new HashMap<>();
+        Map<String, Integer> pokemonMap2 = new HashMap<>();
+        String pokemon = null;
+
+        for(int i = 1; i <= n; i++) {
+            pokemon = br.readLine();
+            pokemonMap1.put(i, pokemon);
+            pokemonMap2.put(pokemon, i);
+        }
+
+        for(int i = 0; i < m; i++) {
+            String tempStr = br.readLine();
+            if(isInteger(tempStr)){ // 숫자일 경우
+                pokemonIdx = Integer.parseInt(tempStr);
+                bw.write(pokemonMap1.get(pokemonIdx) + "\n");
+            } else { // 문자열일 경우
+                bw.write(pokemonMap2.get(tempStr) + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
