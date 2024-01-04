361e59c2f5398196a71839c282197791bcdff23b
ChaeWon Lee <87990439+zzex3on@users.noreply.github.com>
2024-01-04 16:07:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,28 +4,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int n = Integer.parseInt(br.readLine());
         int count = 0;
-
-        if((n >= 10) && (n % 5 == 1)) {
-            n -= 3*2;
-            count = (n/5) + 2;
-        } else if ((n >= 10) && (n % 5 == 2)) {
-            n -= 3*4;
-            count = (n/5) + 4;
-        } else if ((n >= 10) && (n % 5 == 4)) {
-            n -= 3*3;
-            count = (n/5) + 3;
-        } else {
-            count = ((n%5)%3 == 0 ? (n/5) + (n%5)/3 : -1);
+        
+        while (n > 0) {
+            if (n % 5 != 0) {
+                n -= 3;
+                count++;
+            } else {
+                n -= 5;
+                count++;
+            }
         }
 
-        if(count == -1) {
-            count = ((n%3)%5 == 0 ? (n/3) + (n%3)/5 : -1);
-        }
+        if (n != 0) sb.append("-1");
+        else sb.append(count);
 
-        sb.append(count);
         System.out.print(sb);
     }
 }
