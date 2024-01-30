c765b67ffe1cfd49d84211c058f053b5fb95d8b8
ChaeWon Lee <87990439+zzex3on@users.noreply.github.com>
2024-01-30 17:25:32 +0900

    [Silver IV] Title: 수 찾기, Time: 696 ms, Memory: 42836 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = null;
+        int result = 0;
+
+        int n = Integer.parseInt(br.readLine());
+        int arrA[] = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arrA[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arrA); // 배열 정렬해야 이분 탐색 알고리즘 적용 가능
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++) {
+            sb.append(binarySearch(arrA, Integer.parseInt(st.nextToken()), 0, n-1)).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    public static int binarySearch(int[] arrA, int target, int start, int end) {
+        if(start > end)
+            return 0;
+        int mid = (start + end) / 2;
+
+        if(arrA[mid] == target)
+            return 1;
+        if(arrA[mid] < target) {
+            return binarySearch(arrA, target, mid + 1, end);
+        } else {
+            return binarySearch(arrA, target, start, mid - 1);
+        }
+    }
+}
