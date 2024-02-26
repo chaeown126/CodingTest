46377aab7e111a620ae600e21062f1022b80788c
ChaeWon Lee <87990439+zzex3on@users.noreply.github.com>
2024-02-26 17:42:09 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 524 ms, Memory: 114180 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,7 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static ArrayList<Integer>[] list;
+    static int N, M;
+    static boolean [][] list;
     static boolean visited[];
 
     public static void main(String[] args) throws IOException {
@@ -11,42 +12,38 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int count = 0;
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        list = new ArrayList[n+1];
-        visited = new boolean[n+1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        list = new boolean[N+1][N+1];
+        visited = new boolean[N+1];
 
-        // 각 i번째에 ArrayList 를 초기화 시킴 !!!
-        for(int i = 1; i < n+1; i++) {
-            list[i] = new ArrayList<>();
-        }
-
-        // 여기서 i는 순서에 지장을 주지 않음, 단순히 갯수를 세는 용도이기 때문에 0부터 시작하는거임
-        for(int i = 0; i < m; i++) {
+           for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            list[u].add(v);
-            list[v].add(u);
+            list[u][v] = true;
+            list[v][u] = true;
         }
 
-        for(int i = 1; i < n+1; i++) {
-            if(!visited[i]){
+        for(int i = 1; i <= N; i++) {
+            if(DFS(i)){
                 count++;
-                DFS(i);
             }
         }
         sb.append(count);
         System.out.print(sb);
     }
 
-    static void DFS(int n) {
-        if(visited[n]) {
-            return;
+    private static boolean DFS(int node) {
+        if(visited[node])   return false;
+
+        visited[node] = true;
+        for(int i = 1; i <= N; i++) {
+            if(list[node][i]) {
+                DFS(i);
+            }
         }
-        visited[n] = true;
-        for(int i : list[n]) {
-            if(!visited[i])       DFS(i);
-        }
+
+        return true;
     }
 }
