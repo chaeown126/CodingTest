89f1bdf6d607445421bc21b3adc949febce8eada
Chaewon Lee <87990439+chaeum9@users.noreply.github.com>
2025-09-27 13:35:22 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,15 +1,9 @@
 class Solution {
     public String solution(String phone_number) {
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i = 0; i < (phone_number.length() - 4); i++) {
-            sb.append("*");
-        }
-        
-        for(int i = (phone_number.length() - 4); i < phone_number.length(); i++) {
-            sb.append(phone_number.charAt(i));
-        }
-        
-        return sb.toString();
+         char[] ch = phone_number.toCharArray();
+     for(int i = 0; i < ch.length - 4; i ++){
+         ch[i] = '*';
+     }
+     return String.valueOf(ch);
     }
 }
